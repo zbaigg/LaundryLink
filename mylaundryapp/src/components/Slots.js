1fca001a45973bd5efbdd31ec28981372cadf496
Zainab Baig <zainabbaig@Zainabs-MacBook-Air-2.local>
2023-07-22 01:25:03 -0400

    Taking user input for name and showing it with the bookings

--- mylaundryapp/src/components/Slots.js
+++ mylaundryapp/src/components/Slots.js
@@ -6,7 +6,8 @@ function Slots() {
     const [displaySlots, setDisplaySlots] = useState(false); // State to control when to display slots
     const [selectionConfirmed, setIsSelectionConfirmed] = useState(false); // New state to track if Confirm button is clicked
     const [showConfirmationMessage, setConfirmationMessage] = useState(false);
-    
+    const [nameInput, setInput] = useState("")
+
     //function called when 'Book Available Slot' button is clicked
     function BookAvailableSlots() {
         console.log('Book Available Slots Button clicked');
@@ -33,44 +34,9 @@ function Slots() {
         setConfirmationMessage(true);
     }
 
-//     return (
-//         <div className="App-container">
-//             <div className="App-button-display">
-//                 <button onClick={BookAvailableSlots}>Book Available Slots</button>
-//                 <button onClick={BookedSlots}>Booked Slots</button>
-//             </div>
-//              {displaySlots && ( // Render the slots (2nd operant) only if displaySlots is true 
-//                 <div className="App-output">
-//                     <ul className="no-bullets"> {/*Adding a class to remove bullet points from the output*/}
-//                         {availableSlots.map((slot, index) => ( //mapping is iterating over the array items and then defining what happens to them, in this case making list items (li) for each item in the array
-//                             <li key={index}> {/*making list item*/}
-//                                 <label> {/*to asscoaite text/label with the checkbox, the <label> element encloses the text that is associated with the input type (checkboxes)*/}
-//                                     <input
-//                                         type="checkbox" //type of input field 
-//                                         checked={checkedSlots.includes(slot)}
-//                                         onChange={() => checkboxHandling(slot)} //event handler to call the checkboxHandling function when a user checks or unchecks a box
-//                                     />
-//                                     {slot}
-//                                 </label>
-//                             </li>
-//                         ))}
-//                         {/*how do i display/print the items stored in a state variable array */}
-//                         {/*{checkedSlots.map((prebookedSlots, index) => (
-//                         <li key={index}>{prebookedSlots}</li>*/}
-//                         {/*but i only wanna display when a specific button is clicked*/}
-//                         {/*))}*/}
-//                     </ul>
-//                     <button>Confirm</button> {/*I want to print/display the checkmarked items/items in the checkedSlots state variable after the Booked Slots button is clicked*/}
-//                 </div>
-//             )}
-
-
-//         </div>
-//     );
-// }
-
-// export default Slots;
-
+    function nameInputHandling(inputValue){
+        setInput(inputValue.target.value)
+    }
 
 return (
     <div className="App-container">
@@ -99,8 +65,14 @@ return (
                     {/*but i only wanna display when a specific button is clicked*/}
                     {/*))}*/}
                 </ul>
+                <input 
+                    type='text'
+                    value={nameInput}
+                    onChange={nameInputHandling} 
+                />
+
                 <button onClick={ConfirmHandling}>Confirm</button> {/*I want to print/display the checkmarked items/items in the checkedSlots state variable after the Booked Slots button is clicked*/}
-                {showConfirmationMessage && <p>Booking has been confirmed!</p>}
+                {showConfirmationMessage && <p>Booking for {nameInput} has been confirmed!</p>}
             </div>
         ):(
             //when i click on Booked Slots button, the following code output is rendered
@@ -110,7 +82,7 @@ return (
                 <ul className="no-bullets">
 
                     {checkedSlots.map((prebookedSlots, index) => (
-                        <li key={index}>{prebookedSlots}</li>
+                        <li key={index}>{prebookedSlots} (Booked by: {nameInput})</li>
                         // {/*but i only wanna display when a specific button is clicked - Done*/}
             
                     ))}
