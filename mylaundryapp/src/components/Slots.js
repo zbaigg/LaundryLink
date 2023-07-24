1d6379b58a44d34ba9d0cf4d7f4d760169154b66
Zainab Baig <zainabbaig@Zainabs-MacBook-Air-2.local>
2023-07-24 04:35:43 -0400

    Changing the output to a more readable format, allowing multiple entries by the same user and different users, setting logistics for start and end times.

--- mylaundryapp/src/components/Slots.js
+++ mylaundryapp/src/components/Slots.js
@@ -1,21 +1,121 @@
+// import React, { useState } from 'react';
+// import DateTimePicker from 'react-datetime-picker';
+// import 'react-datetime-picker/dist/DateTimePicker.css';
+// import 'react-calendar/dist/Calendar.css';
+// import 'react-clock/dist/Clock.css';
+
+// function Slots() {
+//   const [displaySlots, setDisplaySlots] = useState(false);
+//   const [showConfirmationMessage, setConfirmationMessage] = useState(false);
+//   const [nameInput, setInput] = useState('');
+//   const [timeSlotsArray, setTimeSlotsArray] = useState([]);
+//   const [startDateTime, setStartDateTime] = useState(new Date());
+//   const [endDateTime, setEndDateTime] = useState(new Date());
+
+//   function BookAvailableSlots() {
+//     console.log('Book Available Slots Button clicked');
+//     setDisplaySlots(true);
+//     setConfirmationMessage(false); // Reset confirmation message when showing booking interface
+//   }
+
+//   function BookedSlots() {
+//     console.log('Booked Slots Button clicked');
+//     setDisplaySlots(false);
+//   }
+
+//   function ConfirmHandling() {
+//     if (startDateTime >= endDateTime) {
+//       alert('Error: Start time must be earlier than end time.');
+//       return;
+//     }
+  
+//     const newTimeSlot = {
+//       userName: nameInput,
+//       userStartDateTime: startDateTime,
+//       userEndDateTime: endDateTime,
+//     };
+  
+//     setTimeSlotsArray([...timeSlotsArray, newTimeSlot]);
+//     setConfirmationMessage(true);
+//   }
+  
+
+//   function nameInputHandling(inputValue) {
+//     setInput(inputValue.target.value);
+//   }
+
+//   function enterAnotherEntry() {
+//     setInput('');
+//     setStartDateTime(new Date());
+//     setEndDateTime(new Date());
+//     setConfirmationMessage(false); // Reset confirmation message when entering another entry
+//   }
+
+//   return (
+//     <div className="App-container">
+//       <div className="App-button-display">
+//         <button onClick={BookAvailableSlots}>Book Available Slots</button>
+//         <button onClick={BookedSlots}>Booked Slots</button>
+//       </div>
+//       {displaySlots ? (
+//         <div className="App-output">
+//           <DateTimePicker onChange={setStartDateTime} value={startDateTime} />
+//           <DateTimePicker onChange={setEndDateTime} value={endDateTime} />
+//           <div className="input-container">
+//             <label htmlFor="nameInput">Enter your name:</label>
+//             <input type="text" value={nameInput} onChange={nameInputHandling} />
+//           </div>
+//           <button onClick={ConfirmHandling}>Confirm</button>
+//           {showConfirmationMessage && (
+//             <div>
+//               <p>
+//                 Booking for {nameInput} from {startDateTime.toISOString()} to {endDateTime.toISOString()} has been
+//                 confirmed!
+//               </p>
+//               <button onClick={enterAnotherEntry}>Enter Another Entry</button>
+//             </div>
+//           )}
+//         </div>
+//       ) : (
+//         timeSlotsArray.length > 0 && (
+//           <div>
+//             <h2>Booked Slots:</h2>
+//             <ul className="no-bullets">
+//               {timeSlotsArray.map((timeSlot, index) => (
+//                 <li key={index}>
+//                   {timeSlot.userStartDateTime.toISOString()} to {timeSlot.userEndDateTime.toISOString()} (Booked by:{' '}
+//                   {timeSlot.userName})
+//                 </li>
+//               ))}
+//             </ul>
+//           </div>
+//         )
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Slots;
+
+
 import React, { useState } from 'react';
 import DateTimePicker from 'react-datetime-picker';
 import 'react-datetime-picker/dist/DateTimePicker.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-clock/dist/Clock.css';
-// import { format } from 'date-fns'; // Import the format function from date-fns
 
 function Slots() {
   const [displaySlots, setDisplaySlots] = useState(false);
-  const [selectionConfirmed, setIsSelectionConfirmed] = useState(false);
   const [showConfirmationMessage, setConfirmationMessage] = useState(false);
   const [nameInput, setInput] = useState('');
+  const [timeSlotsArray, setTimeSlotsArray] = useState([]);
   const [startDateTime, setStartDateTime] = useState(new Date());
   const [endDateTime, setEndDateTime] = useState(new Date());
 
   function BookAvailableSlots() {
     console.log('Book Available Slots Button clicked');
     setDisplaySlots(true);
+    setConfirmationMessage(false); // Reset confirmation message when showing booking interface
   }
 
   function BookedSlots() {
@@ -24,28 +124,46 @@ function Slots() {
   }
 
   function ConfirmHandling() {
-    setIsSelectionConfirmed(true);
+    if (startDateTime >= endDateTime) {
+      alert('Error: Start time must be earlier than end time.');
+      return;
+    }
+  
+    const newTimeSlot = {
+      userName: nameInput,
+      userStartDateTime: startDateTime,
+      userEndDateTime: endDateTime,
+    };
+  
+    setTimeSlotsArray([...timeSlotsArray, newTimeSlot]);
     setConfirmationMessage(true);
   }
+  
 
   function nameInputHandling(inputValue) {
     setInput(inputValue.target.value);
   }
 
-//   // Function to handle changes in the start time
-//   function handleStartDateTimeChange(dateTime) {
-//     // Format the dateTime before saving it to the state
-//     const formattedDateTime = format(dateTime, "yyyy-MM-dd hh:mm a");
-//     setStartDateTime(formattedDateTime);
-//   }
+  function enterAnotherEntry() {
+    setInput('');
+    setStartDateTime(new Date());
+    setEndDateTime(new Date());
+    setConfirmationMessage(false); // Reset confirmation message when entering another entry
+  }
 
-//   // Function to handle changes in the end time
-//   function handleEndDateTimeChange(dateTime) {
-//     // Format the dateTime before saving it to the state
-//     const formattedDateTime = format(dateTime, "yyyy-MM-dd hh:mm a");
-//     setEndDateTime(formattedDateTime);
-//   }
+  function formatDateTime(dateTime) {
+    const options = {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    };
+    return new Date(dateTime).toLocaleString('en-US', options);
+  }
   
+  
+
   return (
     <div className="App-container">
       <div className="App-button-display">
@@ -54,37 +172,34 @@ function Slots() {
       </div>
       {displaySlots ? (
         <div className="App-output">
-          <DateTimePicker
-            onChange={setStartDateTime}
-            value={startDateTime}
-          />
-          <DateTimePicker
-            onChange={setEndDateTime}
-            value={endDateTime}
-          />
+          <DateTimePicker onChange={setStartDateTime} value={startDateTime} />
+          <DateTimePicker onChange={setEndDateTime} value={endDateTime} />
           <div className="input-container">
             <label htmlFor="nameInput">Enter your name:</label>
-            <input
-              type="text"
-              value={nameInput}
-              onChange={nameInputHandling}
-            />
+            <input type="text" value={nameInput} onChange={nameInputHandling} />
           </div>
           <button onClick={ConfirmHandling}>Confirm</button>
           {showConfirmationMessage && (
-            <p>
-              Booking for {nameInput} from{' '}
-              {startDateTime.toISOString()} to {endDateTime.toISOString()} has been confirmed!
-            </p>
+            <div>
+              <p>
+              Booking for {nameInput} from {formatDateTime(startDateTime)} to {formatDateTime(endDateTime)} has been confirmed!
+              </p>
+              <button onClick={enterAnotherEntry}>Enter Another Entry</button>
+            </div>
           )}
         </div>
       ) : (
-        selectionConfirmed && (
-          <ul className="no-bullets">
-            <li>
-              {startDateTime.toISOString()} to {endDateTime.toISOString()} (Booked by: {nameInput})
-            </li>
-          </ul>
+        timeSlotsArray.length > 0 && (
+          <div>
+            <h2>Booked Slots:</h2>
+            <ul className="no-bullets">
+                {timeSlotsArray.map((timeSlot, index) => (
+                    <li key={index}>
+                    {formatDateTime(timeSlot.userStartDateTime)} to {formatDateTime(timeSlot.userEndDateTime)} (Booked by: {timeSlot.userName})
+                    </li>
+                ))}
+            </ul>
+          </div>
         )
       )}
     </div>
