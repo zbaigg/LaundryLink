a9b9e6c6f4a9906fbf9e674bc986a9d9e2d9b790
Zainab Baig <zainabbaig@Zainabs-MacBook-Air-2.local>
2023-07-25 19:16:59 -0400

    added 'contactme' and 'about' endpoints using react routing

--- mylaundryapp/src/components/Contact.js
+++ mylaundryapp/src/components/Contact.js
@@ -3,12 +3,16 @@ import React from 'react';
 function ContactMe() {
   return (
     <div>
-      <h1>About Me</h1>
-      <p1>Hey there! I'm Zainab Baig, a determined university student currently in my 3rd year of pursuing a Bachelor of Computer Science (ICS) degree. Born and brought up in Pakistan, I had the incredible opportunity to complete my high schooling and undergraduate education in Canada at Queen's University, where I majored in Life Science. However, my passion for technology and the desire to make a meaningful impact led me to an exciting new path—I'm thrilled to be starting my Bachelor of Computer Science journey in September 2023.
-      </p1>
+      <h1>About the Creator</h1>
+      <p>
+      Hey there! I'm Zainab Baig, a determined university student currently in my 3rd year of pursuing a Bachelor of Computer Science (ICS) degree. Born and brought up in Pakistan, I had the incredible opportunity to complete my high schooling and undergraduate education in Canada at Queen's University, where I majored in Life Science. As I finished my undergraduate studies in 2023, my passion for technology and the desire to make a meaningful impact led me to an exciting new path—I'm thrilled to be starting my Bachelor of Computer Science journey at the University of British Columbia.
+
+      </p>
+      <p>
+      If you have any questions or concerns, feel free to contact me at zainabbaig0@gmail.com
+      </p>
     </div> 
   )
 }
-
 
 export default ContactMe;
